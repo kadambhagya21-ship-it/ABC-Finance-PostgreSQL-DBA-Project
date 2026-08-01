3380955e1d1b5ba0a633115b2cf52dbe5f9e4e78
BHAGYASHREE KADAM <kadambhagya21@gmail.com>
2026-08-01 18:11:06 +0100

    Create ABCFinanceDB with existence check
    This script checks for the existence of the database and drops it if it exists before creating a new one.

--- 01_SQL_Server/01_Create_Database.sql
+++ 01_SQL_Server/01_Create_Database.sql
@@ -1,1 +1,26 @@
+01_Create_Database.sql
+/*
+==========================================
+Project : ABC Finance Banking Database
+Module 1 : Create Database
+Author  : Bhagyashree Kadam
+Database: SQL Server 2022
+==========================================
+*/
 
+-- Check if database already exists
+IF DB_ID('ABCFinanceDB') IS NOT NULL
+BEGIN
+    DROP DATABASE ABCFinanceDB;
+END;
+GO
+
+-- Create Database
+CREATE DATABASE ABCFinanceDB;
+GO
+
+-- Use Database
+USE ABCFinanceDB;
+GO
+
+PRINT 'ABCFinanceDB database created successfully.';
