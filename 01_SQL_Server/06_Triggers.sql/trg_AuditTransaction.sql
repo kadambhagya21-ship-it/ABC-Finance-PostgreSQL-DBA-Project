ee44c1fb5fafcc1b19ff62e40ab6036caa2fd159
BHAGYASHREE KADAM <kadambhagya21@gmail.com>
2026-08-02 16:19:53 +0100

    Add trg_AuditTransaction trigger for transaction auditing
    Creates an audit trigger to record new transactions in the Transactions table.

--- 01_SQL_Server/06_Triggers.sql/trg_AuditTransaction.sql
+++ 01_SQL_Server/06_Triggers.sql/trg_AuditTransaction.sql
@@ -1,1 +1,39 @@
 
+==========================================================
+Project      : ABCFinanceDB
+File Name    : 02_trg_AuditTransaction.sql
+Object Type  : Trigger
+
+Description:
+Creates an audit trigger that automatically records
+new transactions inserted into the Transactions table.
+
+Trigger Event:
+AFTER INSERT
+
+Purpose:
+- Maintain transaction audit history
+- Track database activities
+- Improve monitoring and compliance
+
+==========================================================
+
+
+CREATE TRIGGER trg_AuditTransaction
+ON Transactions
+AFTER INSERT
+AS
+BEGIN
+
+    INSERT INTO TransactionAudit
+    (
+        TransactionID,
+        ActionPerformed
+    )
+    SELECT
+        TransactionID,
+        'New Transaction'
+    FROM inserted;
+
+END;
+GO
