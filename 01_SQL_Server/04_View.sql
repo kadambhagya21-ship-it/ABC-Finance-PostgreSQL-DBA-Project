65f195ab5b75df397609050fe5682bc80a07a020
BHAGYASHREE KADAM <kadambhagya21@gmail.com>
2026-08-02 15:00:43 +0100

    Create views for various financial summaries
    Added multiple views for customer, branch, loan, employee, credit card, and account balance summaries.

--- 01_SQL_Server/04_View.sql
+++ 01_SQL_Server/04_View.sql
@@ -36,11 +36,143 @@ The views in this project support common banking operations such as customer acc
 - Provides consistent reporting
 - Makes application development easier
 - Enhances data readability
-
+************************************************************
 ### Example
-
-```sql
+***************************************************************
+ - Customer Account Summary
+*************************************************************
+CREATE VIEW vw_CustomerAccountSummary
+AS
+SELECT
+    c.CustomerID,
+    c.FirstName,
+    c.LastName,
+    a.AccountID,
+    a.AccountNumber,
+    a.AccountType,
+    a.Balance,
+    b.BranchName
+FROM Customers c
+JOIN Accounts a
+    ON c.CustomerID = a.CustomerID
+JOIN Branches b
+    ON a.BranchID = b.BranchID;
+GO
+  
 SELECT * FROM vw_CustomerAccountSummary;
-```
 
+*****************************************************************
+  - Customer Transaction Summary
+****************************************************************
+CREATE VIEW vw_CustomerTransactionSummary
+AS
+SELECT
+    c.CustomerID,
+    c.FirstName,
+    c.LastName,
+    
+    t.TransactionID,
+    t.TransactionType,
+    t.Amount,
+    t.TransactionDate
+FROM Customers c
+JOIN Accounts a
+    ON c.CustomerID = a.CustomerID
+JOIN Transactions t
+    ON a.AccountID = t.AccountID;
+GO
+  
+SELECT * FROM vw_CustomerTransactionSummary;
+*****************************************************************
+- Branch Account Summary
+******************************************************************
+  CREATE VIEW vw_BranchAccountSummary
+AS
+SELECT
+    b.BranchID,
+    b.BranchName,
+    COUNT(a.AccountID) AS TotalAccounts,
+    SUM(a.Balance) AS TotalBalance
+FROM Branches b
+LEFT JOIN Accounts a
+    ON b.BranchID = a.BranchID
+GROUP BY
+    b.BranchID,
+    b.BranchName;
+GO
+  
+select * from vw_BranchAccountSummary;
+****************************************************************
+  - Loan Details
+************************************************************
+CREATE VIEW vw_LoanDetails
+AS
+SELECT
+    l.LoanID,
+    c.CustomerID,
+    c.FirstName,
+    c.LastName,
+    l.LoanType,
+    l.LoanAmount,
+    l.InterestRate,
+    l.LoanStatus
+FROM Loans l
+JOIN Customers c
+    ON l.CustomerID = c.CustomerID;
+GO
+  
+SELECT * FROM vw_LoanDetails;
+**********************************************************8
+- Employee Branch Details
+**********************************************************
+CREATE VIEW vw_EmployeeBranchDetails
+AS
+SELECT
+    e.EmployeeID,
+    e.FirstName,
+    e.LastName,
+    e.JobTitle,
+    b.BranchName,
+    b.City
+FROM Employees e
+JOIN Branches b
+    ON e.BranchID = b.BranchID;
+GO
+SELECT * FROM vw_EmployeeBranchDetails;
+*******************************************************
+  - Credit Card Details
+*********************************************************
+CREATE VIEW vw_CreditCardDetails
+AS
+SELECT
+    cc.CardID,
+    cc.CardNumber,
+   
+    cc.ExpiryDate,
+    c.CustomerID,
+    c.FirstName,
+    c.LastName
+FROM CreditCards cc
+JOIN Customers c
+    ON cc.CustomerID = c.CustomerID;
+GO
+  
+SELECT * FROM vw_CreditCardDetails;
+*************************************************
+  - Account Balance Summary
+**************************************************
+CREATE VIEW vw_AccountBalanceSummary
+AS
+SELECT
+    AccountType,
+    COUNT(AccountID) AS TotalAccounts,
+    SUM(Balance) AS TotalBalance,
+    AVG(Balance) AS AverageBalance,
+    MIN(Balance) AS MinimumBalance,
+    MAX(Balance) AS MaximumBalance
+FROM Accounts
+GROUP BY AccountType;
+GO
+SELECT * FROM vw_AccountBalanceSummary;
+**************************************************  
 This script is part of **Module 2 – Views, Stored Procedures, Triggers & Indexes** of the ABCFinanceDB SQL Server project.
