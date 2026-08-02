a0ea611297ef702a259ef92ecbae2f1b760cd650
BHAGYASHREE KADAM <kadambhagya21@gmail.com>
2026-08-02 16:18:01 +0100

    Remove TransactionAudit table creation from triggers
    Removed the creation of the TransactionAudit table and its description.

--- 01_SQL_Server/06_Triggers.sql/01_triggersdetails.sql
+++ 01_SQL_Server/06_Triggers.sql/01_triggersdetails.sql
@@ -37,21 +37,4 @@ Prerequisites:
 4. trg_PreventNegativeBalance.sql
    - Prevents invalid account balances.
     
-====================================================================================================
-1. Create Transaction Audit Table
-=====================================================================================================
-
-CREATE TABLE TransactionAudit
-(
-    AuditID INT IDENTITY(1,1) PRIMARY KEY,
-    TransactionID INT,
-    ActionPerformed VARCHAR(50),
-    ActionDate DATETIME DEFAULT GETDATE()
-);
-GO
-
-  Description:
-Stores a record of every transaction inserted into the
-Transactions table, including the transaction ID,
-action performed, and timestamp.
-=======================================================================================================
+============================================================
