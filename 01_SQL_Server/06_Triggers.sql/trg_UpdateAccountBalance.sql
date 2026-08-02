f0a041512695469969d42ea04221d451066a00d7
BHAGYASHREE KADAM <kadambhagya21@gmail.com>
2026-08-02 16:20:31 +0100

    Create trigger to update account balance on transactions
    This trigger updates account balances after deposit or withdrawal transactions.

--- 01_SQL_Server/06_Triggers.sql/trg_UpdateAccountBalance.sql
+++ 01_SQL_Server/06_Triggers.sql/trg_UpdateAccountBalance.sql
@@ -1,1 +1,45 @@
 
+==========================================================
+Project      : ABCFinanceDB
+File Name    : 03_trg_UpdateAccountBalance.sql
+Object Type  : Trigger
+
+Description:
+This trigger automatically updates the account balance
+when a new deposit or withdrawal transaction is inserted.
+
+Trigger Event:
+AFTER INSERT on Transactions
+
+Business Rules:
+- Deposit increases account balance
+- Withdrawal decreases account balance
+
+==========================================================
+
+CREATE TRIGGER trg_UpdateAccountBalance
+ON Transactions
+AFTER INSERT
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    UPDATE a
+    SET 
+        a.Balance =
+        CASE 
+            WHEN i.TransactionType = 'Deposit'
+            THEN a.Balance + i.Amount
+
+            WHEN i.TransactionType = 'Withdrawal'
+            THEN a.Balance - i.Amount
+
+            ELSE a.Balance
+        END
+    FROM Accounts a
+    INNER JOIN inserted i
+        ON a.AccountID = i.AccountID;
+
+END;
+GO
