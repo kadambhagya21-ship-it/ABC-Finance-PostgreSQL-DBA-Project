b71e555cbd6973dddcdbfdc106eb1e9512a51fc0
BHAGYASHREE KADAM <kadambhagya21@gmail.com>
2026-08-02 16:34:34 +0100

    Create trigger to prevent negative account balance
    This trigger prevents withdrawal transactions when the account does not have sufficient balance. It raises an error if the balance is insufficient for the requested withdrawal amount.

--- 01_SQL_Server/06_Triggers.sql/trg_PreventNegativeBalance.sql
+++ 01_SQL_Server/06_Triggers.sql/trg_PreventNegativeBalance.sql
@@ -1,1 +1,82 @@
 
+/*
+==========================================================
+Project      : ABCFinanceDB
+File Name    : 04_trg_PreventNegativeBalance.sql
+Object Type  : Trigger
+
+Description:
+This trigger prevents withdrawal transactions when the
+account does not have sufficient balance.
+
+Trigger Event:
+INSTEAD OF INSERT on Transactions
+
+Business Rule:
+Account balance cannot become negative.
+
+==========================================================
+*/
+
+CREATE TRIGGER trg_PreventNegativeBalance
+ON Transactions
+INSTEAD OF INSERT
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    IF EXISTS
+    (
+        SELECT 1
+        FROM inserted i
+        INNER JOIN Accounts a
+            ON i.AccountID = a.AccountID
+        WHERE i.TransactionType = 'Withdrawal'
+        AND a.Balance < i.Amount
+    )
+    BEGIN
+        RAISERROR
+        (
+            'Transaction failed: Insufficient account balance.',
+            16,
+            1
+        );
+
+        RETURN;
+    END;
+
+
+    INSERT INTO Transactions
+    (
+        AccountID,
+        TransactionType,
+        Amount,
+        TransactionDate
+    )
+    SELECT
+        AccountID,
+        TransactionType,
+        Amount,
+        TransactionDate
+    FROM inserted;
+
+END;
+========================================================================================================
+  test data
+===========================================================================================================
+  INSERT INTO Transactions
+(
+    AccountID,
+    TransactionType,
+    Amount,
+    TransactionDate
+)
+VALUES
+(
+    10001,
+    'Withdrawal',
+    999999,
+    GETDATE()
+);
+GO
